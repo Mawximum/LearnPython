826f61fb99c932ceffe2fd1d983d8aea2395b00e
Mawximum <84628557+Mawximum@users.noreply.github.com>
2021-06-05 16:46:04 +0300

    Update Prac-02.py
    Testing resolving conflict n merge

--- Prac-02.py
+++ Prac-02.py
@@ -1,2 +1,3 @@
 print ("Welcome to Python Practice File N0. 02")
 print ("this is new line added from Github - remote repository")
+print ("this is the line added here to test how to resolve conflict")
